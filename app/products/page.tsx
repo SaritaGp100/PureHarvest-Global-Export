40891591c6824e421f48d1ab47733e58397403a4
Saritagp100 <Saritagp100@gmail.com>
2026-06-18 14:13:10 +0530

    Fix header

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,7 +3,7 @@ import ProductCard from "@/components/ui/ProductCard";
 
 export default function ProductsPage() {
   return (
-    <section className="py-24">
+    <section className="py-10">
       <div className="max-w-7xl mx-auto px-5">
 
         <h1 className="text-5xl font-bold mb-12">
